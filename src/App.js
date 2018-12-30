4a07fc9b79e249292b2b30f0775e39da70764361
Paweł Stanecki <pawelstanecki@Offices-MBP.home>
2018-12-30 15:22:10 +0100

    Added some files and prepare to add content of the asyncComponent to use lazy load

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import Users from './Containers/Users';
+import Pizza from './Containers/Pizza';
+
+class App extends Component{
+    render() {
+        return (
+            <div>
+                <div>
+                    <Link to="/">Users</Link> |
+                    <Link to="/pizza">Pizza</Link>
+                </div>
+                <div>
+                    <Route path="/" exact component={Users} />
+                    <Route path="/pizza" component={Pizza} />
+                </div>
+            </div>
+        );
+    }
+}
